1961b24431d68f1b9a73f8b097cc5c1fc2637cec
tmarinkovic <tmarin@thoughtworks.com>
2019-04-22 13:37:00 +0200

    Start version from 0

--- script/destroy-resources.sh
+++ script/destroy-resources.sh
@@ -2,7 +2,7 @@
 AWS_PROFILE=$1
 PROJECT_NAME=$2
 
-echo "version=1" > version.properties
+echo "version=0" > version.properties
 
 aws ecr delete-repository --force --repository-name "$PROJECT_NAME"-docker-repo --profile "$AWS_PROFILE"
 aws cloudformation delete-stack --stack-name "$PROJECT_NAME" --region eu-west-1 --profile "$AWS_PROFILE"
